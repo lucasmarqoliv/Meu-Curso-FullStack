60f0530209cc312fa54c5c0c4b9c5652f40800eb
Lucas Marques de Oliveira <lucasmarqoliv@gmail.com>
2024-08-23 11:04:08 -0300

    up

--- Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Praticas/eventos do teclado/script.js
+++ Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Praticas/eventos do teclado/script.js
@@ -8,13 +8,27 @@ input01.addEventListener('keydown', function() {
 //----------------------------------------------------------
 
 let input02 = document.getElementById('input02');
-
 input02.addEventListener('keydown', function(evento) {
     return teclaEnter(evento);
 });
 
 function teclaEnter(evento) {
-    if (evento.key === "Enter") { // Propriedade key: Essa propriedade retorna uma string que representa a tecla que foi pressionada pelo usuário. Por exemplo, se o usuário pressionar a tecla "Enter", evento.key retornará "Você clicou no "ENTER!".
-        alert('Você clicou no "ENTER!')
+    if (evento.key === "Enter + ") { // Propriedade key: Essa propriedade retorna uma string que representa a tecla que foi pressionada pelo usuário. Por exemplo, se o usuário pressionar a tecla "Enter", evento.key retornará "Você clicou no "ENTER!".
+        alert('Você clicou no "ENTER!');
     };
 };
+
+//---------------------------------------------------------
+// NÃO FUNCIONA AINDA
+
+let input03 = document.getElementById('input03'); 
+input03.addEventListener('keypress', function(evento){
+    return teclaShift(evento);
+});
+
+function teclaShift(evento) {
+    if (evento.key === "" && "Enter") {
+        alert ('teste');
+    };
+};
+
