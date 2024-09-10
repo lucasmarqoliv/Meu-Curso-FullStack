11620378d624547c50ba37435dda88aaa6bdb699
Lucas Marques de Oliveira <lucasmarqoliv@gmail.com>
2024-09-10 09:54:58 -0300

    update

--- Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Praticas/exercicios array eventos if else/calculadora_simples.js
+++ Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Praticas/exercicios array eventos if else/calculadora_simples.js
@@ -1,3 +1,26 @@
+//6. Calculadora Simples: Crie uma calculadora que permite ao usuário inserir dois números e escolher uma operação (+, -, *, /) a partir de um menu suspenso. Use um botão para calcular o resultado e exibi-lo em um elemento de resultado.
 
-let numero_1 = document.getElementById('numero_1')
-let numero_2 = document.getElementById('numero_2')
+
+let calcular = document.getElementById('btn_calcular');
+
+calcular.addEventListener('click', function(event){
+    event.preventDefault()
+    let num1 = parseFloat(document.getElementById('numero_1').value);
+    let num2 = parseFloat(document.getElementById('numero_2').value);
+    let operacao = document.getElementById('operacao').value;
+    let resultado;
+    if (operacao === '+') {
+        resultado = num1 + num2;
+    }
+    else if (operacao === '-') {
+        resultado = num1 - num2;
+    }
+    else if (operacao === '*') {
+        resultado = num1 * num2;
+    }
+    else if (operacao === '/') {
+        resultado = num1 / num2;
+    }
+    document.getElementById('resultado').value = resultado;  // Exibe o resultado
+
+})
