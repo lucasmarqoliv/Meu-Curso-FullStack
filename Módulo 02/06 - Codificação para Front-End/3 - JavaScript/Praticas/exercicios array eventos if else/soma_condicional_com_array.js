b793643c22b86ff5c57d6984718076ba3631a100
Lucas Marques de Oliveira <lucasmarqoliv@gmail.com>
2024-09-12 10:39:22 -0300

    update

---
+++ Módulo 02/06 - Codificação para Front-End/3 - JavaScript/Praticas/exercicios array eventos if else/soma_condicional_com_array.js
@@ -0,0 +1,21 @@
+// 7. Soma Condicional com Array: Crie um array de números e um botão. Quando o botão for clicado, calcule a soma de todos os números que são maiores que um determinado valor. O valor de referência deve ser inserido em um campo de entrada.
+
+
+    let numeros = [10, 5, 7, 8, 9, 6, 4, 11];
+
+    let div = document.getElementById('div') ;
+    div.textContent = (`Array de numeros: ${numeros.join(", ")}`) 
+
+    let calcular = document.getElementById('btn');
+
+    calcular.addEventListener('click', function(event){
+        event.preventDefault()
+        let numero_digitado = parseFloat(document.getElementById('input').value);
+        let resultado = document.getElementById('resultado')
+        let numeros_maiores = numeros.filter(numero => 
+            numero > numero_digitado)
+        numeros_maiores = numeros_maiores.reduce((acumulador, numero) => acumulador + numero, 0)
+        resultado.value = numeros_maiores;
+    })
+        
+
